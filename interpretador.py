599df5d959faf9620ee2f6aefed7a7e1ef47def6
TiagoRCarneiro74 <112009684+TiagoRCarneiro74@users.noreply.github.com>
2023-11-14 14:54:41 -0300

    Função 'inserir em'

--- interpretador.py
+++ interpretador.py
@@ -71,10 +71,53 @@ def interpreta_comando(comando, db, executando):
             # Exemplos de sintaxe válida:
             # criar tabela Teste: campo1, campo2.
             # criar tabela Teste : campo1 campo2.
-            # criar tabela Teste: cam,po1, cam.po2.
+            # criar tabela Teste: cam,po1, cam.po2
             # Se a nova tabela tiver o mesmo nome que outra já existente,
             # a nova tabela não será criada
             # e o programa esperará a próxima instrução.
+
+
+    elif palavras_do_comando[0] == 'inserir':
+        if palavras_do_comando[1] == 'em':
+            i = 4
+            if palavras_do_comando[3] == ':' \
+                    or palavras_do_comando[2][len(palavras_do_comando[2])-1] == ':':
+                if palavras_do_comando[2][len(palavras_do_comando[2])-1] == ':':
+                    palavras_do_comando[2] = palavras_do_comando[2][:-1]
+                    i = 3
+                arq_tabela = open("tabelas/" + palavras_do_comando[2] + ".csv", 'a+')
+                pos = arq_tabela.tell()
+                arq_tabela.seek(0)
+                campos = arq_tabela.readline()   # Primeira linha com os campos
+
+                if campos == '':
+                    print("ERRO: Esta tabela está vazia.")
+                    return 1
+                campos = campos.split(sep=",")
+                print((campos))
+                x = 0
+                while x < len(campos):
+                    if campos[x][len(campos[x]) - 1] == ',' or campos[x][len(campos[x]) - 1] == '\n':
+                        campos[x] = campos[x][:-1]
+                    x = x + 1
+                print(len(campos))
+                if len(palavras_do_comando) - i != len(campos):
+                    print("ERRO: Número de argumentos não corresponde ao número de campos.")
+                    return 1
+                arq_tabela.seek(pos)
+                while i < len(palavras_do_comando):
+                    if palavras_do_comando[i][len(palavras_do_comando[i]) - 1] == ',':
+                        palavras_do_comando[i] = palavras_do_comando[i][:-1]
+                    arq_tabela.write(palavras_do_comando[i])
+                    if palavras_do_comando[i] != palavras_do_comando[len(palavras_do_comando)-1]:
+                        arq_tabela.write(',')
+                    else:
+                        arq_tabela.write('\n')
+                    i = i + 1
+            else:
+                erro = 4
+        else:
+            erro = 2
 
     elif palavras_do_comando[0] == 'sair':
         executando[0] = False
